add40b01b6f091e238b86fed44bbbab7c33901c6
themn <mt.lroc@outlook.com>
2020-07-01 17:27:00 +0430

    learn components

---
+++ themn/components/src/CC.js
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+class CC extends Component{
+    render() {
+        return (
+            <h1>this is class component</h1>
+        )
+    }
+}
+export default CC
---
+++ themn/components/src/FC.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function FC() {
+    return(
+        <h1>this is a function component</h1>
+    )
+}
+export default FC
